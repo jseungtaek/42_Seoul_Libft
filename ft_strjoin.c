c49508d26756e1787cb5073b451a3ba55c56cf6f
jseungtaek <tmdxor30@gmail.com>
2022-01-07 15:12:28 +0900

    libft

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 21:26:20 by sejeon            #+#    #+#             */
-/*   Updated: 2022/01/07 14:57:28 by sejeon           ###   ########.fr       */
+/*   Updated: 2022/01/07 15:12:21 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (ft_strdup(s1));
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
+	ret = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (ret == 0)
+		return (0);
 	ft_strlcpy(ret, s1, s1_len + 1);
 	ft_strlcat(ret + s1_len, s2, s2_len + 1);
 	return (ret);
