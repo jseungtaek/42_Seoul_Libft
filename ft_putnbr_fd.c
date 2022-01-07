c46b469100799cf797b527e1925034628a1e18da
jseungtaek <tmdxor30@gmail.com>
2022-01-07 16:26:41 +0900

    libft

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sejeon <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: sejeon <sejeon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/07 16:20:59 by sejeon            #+#    #+#             */
-/*   Updated: 2022/01/07 16:21:07 by sejeon           ###   ########.fr       */
+/*   Created: 2022/01/07 16:22:33 by sejeon            #+#    #+#             */
+/*   Updated: 2022/01/07 16:25:55 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (fd < 0)
+		return ;
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n *= -1;
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd((n % 10) + '0', fd);
+}
